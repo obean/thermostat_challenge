85301f32989770b59ed710ea7aa1c9015ec4b204
Veronica Lee <veronicavlee@icloud.com>
2020-10-06 15:28:53 +0100

    Add reset, current energy usage and psm off

--- jasmine/jasmine-standalone-3.6.0/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.6.0/spec/thermostatSpec.js
@@ -1,17 +1,19 @@
+'use strict';
 
 describe('Thermostat', function() {
+  var thermostat;
 beforeEach(function() {
-thermostat = new Thermostat
+  thermostat = new Thermostat
 });
   it('returns current temp of 20deg', function() {
-    expect(thermostat.currentTemp).toEqual(20)
+    expect(thermostat.getCurrentTemp()).toEqual(20)
   })
 
   describe('up', function() {
 
     it('increases the temperature', function() {
       thermostat.up();
-      expect(thermostat.currentTemp).toEqual(21)
+      expect(thermostat.getCurrentTemp()).toEqual(21)
     })
     it('will not increase temp above 25 with powersaving on', function() {
       var i = 6;
@@ -19,13 +21,22 @@ thermostat = new Thermostat
         i--;
         thermostat.up();
       }
-      expect(thermostat.currentTemp).toEqual(25)
+      expect(thermostat.getCurrentTemp()).toEqual(25)
+    })
+    it('will not increase tempt above 32 with powersaving off', function() {
+      thermostat.powerSavingOff()
+      var i = 14;
+      while(i > 0){
+        i--;
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemp()).toEqual(32)
     })
   })
   describe('down', function() {
     it('decreases the temp by 1', function() {
       thermostat.down();
-      expect(thermostat.currentTemp).toEqual(19)
+      expect(thermostat.getCurrentTemp()).toEqual(19)
     })
     it('cant reduce temperature below 10', function() {
       var i = 12
@@ -33,8 +44,55 @@ thermostat = new Thermostat
         i--;
         thermostat.down(); 
       }
-      expect(thermostat.currentTemp).toEqual(10);
+      expect(thermostat.getCurrentTemp()).toEqual(10);
     });
+  })
+  describe('powerSavingOff', function() {
+    it('turns the power saving mode off', function() {
+      thermostat.powerSavingOff();
+      expect(thermostat.powerSavingMode).toEqual(false)
+    })
+  })
+
+  describe('powerSavingOn', function() {
+    it('turns the power saving mode on', function() {
+      thermostat.powerSavingOff();
+      thermostat.powerSavingOn();
+      expect(thermostat.powerSavingMode).toEqual(true)
+    })
+  })
+
+  describe('maximumTemp', function() {
+    it('returns 25 if power saving mode is on', function() {
+      expect(thermostat.maximumTemp()).toEqual(25);
+    })
+    it('returns 32 if power saving mode is off', function() {
+      thermostat.powerSavingOff()
+      expect(thermostat.maximumTemp()).toEqual(32);
+    })
+  })
+
+  describe('reset', function() {
+    it('resets the current temperature to 20', function() {
+      thermostat.up()
+      thermostat.reset()
+      expect(thermostat.currentTemp).toEqual(20)
+    })
+  })
+
+  describe('currentEnergyUsage', function() {
+    it('returns low-usage when temperate is < 18', function() {
+      thermostat.currentTemp = 16;
+      expect(thermostat.currentEnergyUsage()).toEqual('low-usage')
+    })
+    it('returns medium-usage when temperate is <= 25', function() {
+      thermostat.currentTemp = 25;
+      expect(thermostat.currentEnergyUsage()).toEqual('medium-usage')
+    })
+    it('returns high-usage when temperate is > 25', function() {
+      thermostat.currentTemp = 26;
+      expect(thermostat.currentEnergyUsage()).toEqual('high-usage')
+    })
   })
   
 });
--- jasmine/jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine/jasmine-standalone-3.6.0/src/thermostat.js
@@ -1,18 +1,47 @@
 class Thermostat {
   constructor() {
    this.currentTemp = 20;
-   this.minimumTemp = 10;
-   this.maximumTemp = 25;
-   this.powerSavingModeOn = true;
+   this.MIN_LIMIT = 10;
+   this.MAX_LIMIT_PSM_ON = 25;
+   this.MAX_LIMIT_PSM_OFF = 32;
+   this.powerSavingMode = true;
   }
   up() {
-    if(this.currentTemp < this.maximumTemp) {
+    if(this.currentTemp < this.maximumTemp()) {
     this.currentTemp++;
     }
   }
   down() {
-    if(this.currentTemp > 10){
+    if(this.currentTemp > this.MIN_LIMIT){
     this.currentTemp--; 
     }
+  }
+  powerSavingOff() {
+    this.powerSavingMode = false;
+  }
+  powerSavingOn() {
+    this.powerSavingMode = true;
+  }
+  reset() {
+    this.currentTemp = 20;
+  }
+  currentEnergyUsage() {
+    if(this.currentTemp < 18) {
+      return 'low-usage'
+    } else if(this.currentTemp <= 25) {
+      return 'medium-usage'
+    } else {
+      return 'high-usage'
+    }
+  }
+  maximumTemp() {
+    if(this.powerSavingMode === true) {
+      return this.MAX_LIMIT_PSM_ON;
+    } else {
+      return this.MAX_LIMIT_PSM_OFF;
+    }
+  }
+  getCurrentTemp() {
+    return this.currentTemp;
   }
 }
